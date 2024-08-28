7cebfda6e27da0a5bac9b06e26c8825c6723ae75
JethyHowell <s.sumalhay.jethelyn@cmu.edu.ph>
2024-08-28 19:37:48 +0800

    Create Jethy push trial.py
    hi er

---
+++ Jethy push trial.py
@@ -0,0 +1,33 @@
+Hi errr this is me jethy !!!!!!! 
+
+try try trial hehehehhe
+
+no git bash huhuhuhuhu
+
+ownfwefnkewfnw'fn
+
+hehehe
+ hi 
+ 
+ 
+ 
+ 
+ 
+ 
+ABCD
+
+EFG
+
+HIJK
+
+
+
+QRST
+
+HEHEHE
+
+mao ra to 
+
+commit ko ni 
+
+heheh
